6b6eee2216d36d59ecaa71ad22fb7a911f8b5e91
Aijaz A. Ansari <aijaz@enoor.com>
2014-10-18 10:27:49 -0500

    Initial Commit

---
+++ TTT2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TTT2
+//
+//  Created by Aijaz Ansari on 10/18/14.
+//  Copyright (c) 2014 Euclid Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TTT2/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TTT2
+//
+//  Created by Aijaz Ansari on 10/18/14.
+//  Copyright (c) 2014 Euclid Software, LLC. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
